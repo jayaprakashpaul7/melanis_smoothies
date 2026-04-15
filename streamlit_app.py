6fde50a01ac67ba9aa0a75e04465c9a9ad4ca9e5
jayaprakashpaul7 <134765188+jayaprakashpaul7@users.noreply.github.com>
2026-04-15 12:57:28 +0530

    Add fruit data fetching from SmoothieFroot API
    Fetch fruit data from SmoothieFroot API and display it in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@
 import streamlit as st 
 from snowflake.snowpark.functions import col
 import requests  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/")  
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 # Write directly to the app.
 st.title(f":cup_with_straw: Cutomize Your Smoothie! :cup_with_straw:")
 st.write(
