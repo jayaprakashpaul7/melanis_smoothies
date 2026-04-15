03c49bad4aa623b7fce317f29093a62f4c9c293a
jayaprakashpaul7 <134765188+jayaprakashpaul7@users.noreply.github.com>
2026-04-15 13:13:27 +0530

    Implement ingredient selection with multiselect
    Added a multiselect widget for ingredient selection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ from snowflake.snowpark.functions import col
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: ',
